019fea1d055702c8ebaf694093915c31b746d04f
kyle-n <kylebnazario@gmail.com>
2019-03-07 15:05:11 -0500

    Implements create user API route

--- api/user.ts
+++ api/user.ts
@@ -3,6 +3,7 @@ import { objectEmpty } from '../utils';
 import { User, ServerError } from '../schemas';
 import { Client } from 'pg';
 import { connectionUrl } from '../db';
+import bcrypt from 'bcrypt';
 
 const client: Client = new Client(connectionUrl);
 client.connect();
@@ -10,35 +11,35 @@ client.connect();
 export const validEmail = (addr: string) => /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i.test(addr);
 export const validPassword = (pw: string) => /^(?=.*\d).{6,}$/gi.test(pw);
 
-export const validateUser = (body: any): Promise<string> => {
-  let status: string = '';
-  if (objectEmpty(body)) status = 'missing both';
-  if (!body.email || !validEmail(body.email)) status = 'missing email';
-  if (!body.password || !validPassword(body.password)) status = 'missing password';
-  if (status) return new Promise((resolve, reject) => reject(status));
+export const validateUser = (body: any, needBoth: boolean): Promise<ServerError> => {
+  let error: ServerError = { status: 200, msg: '' };
+  if (objectEmpty(body)) error = { status: 400, msg: 'Must provide an email address and password' };
+  if (needBoth && (!body.email || !validEmail(body.email))) error = { status: 400, msg: 'Must provide an email address' };
+  if (needBoth && (!body.password || !validPassword(body.password))) error = { status: 400, msg: 'Must provide a password' };
+  if (error.msg) return new Promise((resolve, reject) => reject(error));
   else return new Promise((resolve, reject) => {
     client.query('SELECT id FROM users WHERE email = $1;', [body.email]).then(data => {
-      if (data.rowCount) reject(status);
-      else resolve(status);
+      if (data.rowCount) {
+        error = { status: 409, msg: 'Email address is already taken' };
+        reject(error);
+      }
+      else resolve(error);
     });
   });
 }
 
 export const createUser = (req: express.Request, resp: express.Response): number => {
-
-  // check profile data
-  if (objectEmpty(req.body)) {
-    resp.status(400).send('Must provide an email address and password');
-    return 1;
-  }
-  if (!req.body.email) {
-    resp.status(400).send('Must provide an email address');
-    return 1;
-  }
-  if (!req.body.password) {
-    resp.status(400).send('Must provide a password');
-  }
-
+  validateUser(req.body, true).then(() => {
+    return bcrypt.hash(req.body.password, 10);
+  }).then(hashed => {
+    return client.query(`INSERT INTO users (email, password) VALUES ($1, $1) RETURNING id;`, [req.body.email, hashed]);
+  }).then(data => {
+    return client.query('INSERT INTO list_metadata (user_id, list_table_name, title) VALUES ($1, $2, $3) RETURNING list_table_name;', [data.rows[0].id, Date.now(), 'Played games']);
+  }).then(data => {
+    return client.query('CREATE TABLE $1 (id SERIAL PRIMARY KEY, ranking INTEGER, game_id INTEGER, text TEXT);', [data.rows[0].list_table_name]);
+  }).then(() => {
+    return resp.status(200).send();
+  }).catch(e => resp.status(e.status).send(e.msg));
 
   return 0;
 };
