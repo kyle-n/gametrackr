855eaa7ecf876e7d7e95c462217cfb1f79db5937
Kyle Nazario <kylebnazario@gmail.com>
2019-03-08 16:38:28 -0500

    Adds user login route

--- api/user.ts
+++ api/user.ts
@@ -136,6 +136,27 @@ export const deleteUser = (req: express.Request, resp: express.Response): number
   return 0;
 };
 
+const login = (req: express.Request, resp: express.Response) => {
+  if (!req.body.email || !req.body.password) return resp.status(400).send('Must provide a username and password');
+  let error: ServerError = { ...defaultError };
+  let profile: User;
+  client.query('SELECT id, password FROM users WHERE email = $1;', req.body.email).then(rows => {
+    if (!rows.length) {
+      error = { status: 404, msg: 'Cannot find an account for the requested email address' };
+      throw new Error();
+    }
+    profile = rows[0];
+    return bcrypt.compare(req.body.password, profile.password);
+  }).then(pwMatch => {
+    if (!pwMatch) {
+      error = { status: 403, msg: 'Incorrect password' };
+      throw new Error();
+    }
+    const token = jwt.sign({ id: profile.id, email: req.body.email }, <string>process.env.SECRET_KEY);
+    return resp.status(200).json({ token });
+  }).catch(() => resp.status(error.status).send(error.msg));
+}
+
 const fourHundredNotSpecified = (req: express.Request, resp: express.Response) => resp.status(400).send('Request /api/users/user_id, not /api/users');
 
 export const router: express.Router = express.Router();
@@ -143,6 +164,7 @@ router.route('/').get(fourHundredNotSpecified)
                  .post(createUser)
                  .put(fourHundredNotSpecified)
                  .delete(fourHundredNotSpecified);
+router.route('/login').post(login);
 router.route('/:userId').get(readUser)
                         .put(updateUser)
                         .delete(deleteUser);
