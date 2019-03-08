4cdb6df41bca0fa823195dcc89e6985fb212e9cf
Kyle Nazario <kylebnazario@gmail.com>
2019-03-08 17:03:58 -0500

    Fixes up before/after functions for list test file

--- api/tests/list.spec.ts
+++ api/tests/list.spec.ts
@@ -14,42 +14,46 @@ const defaultError: ServerError = { status: 500, msg: 'Internal server error' };
 const email = process.env.TEST_EMAIL, password = process.env.TEST_PASSWORD;
 const title = 'Test list', deck = 'Test deck';
 let token: string;
+let userId: number;
 
 describe('Router list api interface', () => {
 
   before(done => {
     setTimeout(() => {
-      client.query('SELECT id FROM users WHERE email = $1;', [email]).then(rows => {
-        if (!rows.length) return chai.request(app).post('/api/users').send({ email, password });
-        return client.query('SELECT list_table_name FROM list_metadata WHERE user_id = $1;', [rows[0].id]);
-      }).then(rowOrResp => {
-        if (rowOrResp.status) return setTimeout(done, 500);
-        if (rowOrResp.length === 1 && rowOrResp[0].list_table_name.toLowerCase() === 'played games') return done();
-        for (let i = 0; i < rowOrResp.length; i++) {
-          if (rowOrResp[i].list_table_name.toLowerCase() === 'played games') {
-            if (i === rowOrResp.length - 1) return done();
-            else continue;
-          }
-          client.query('DROP TABLE IF EXISTS $1~;', rowOrResp[i].list_table_name).then(() => {
-            if (i === rowOrResp.length - 1) return done();
+      client.query('SELECT id FROM users WHERE email = $1;', email).then(rows => {
+        // login if there's a test account, otherwise create one
+        if (rows.length) return chai.request(app).post('/api/users/login').send({ email, password });
+        else return chai.request(app).post('/api/users').send({ email, password });
+      }).then(resp => {
+        token = resp.body.token;
+        return client.query('SELECT id FROM users WHERE email = $1;', email);
+      }).then(rows => {
+        userId = rows[0].id;
+        return client.query('DELETE FROM list_metadata WHERE user_id = $1 RETURNING list_table_name;', userId);
+      }).then(rows => {
+        if (!rows.length) return done();
+        rows.forEach((table: any, i: number)=> {
+          client.query('DROP TABLE IF EXISTS $1~;', table.list_table_name).then(() => {
+            if (i === rows.length - 1) return done()
           });
-        }
+        });
       });
     }, 500);
   });
 
   afterEach(done => {
-    client.query('SELECT id FROM users WHERE email = $1;', email).then(rows => {
+    client.query('DELETE FROM list_metadata WHERE user_id = $1 RETURNING list_table_name;', userId).then(rows => {
       if (!rows.length) return done();
-      client.query('SELECT list_table_name FROM list_metadata WHERE user_id = $1;', rows[0].id).then(listRows => {
-        const tablesToDelete = listRows.filter((lr: any) => lr.list_table_name.toLowerCase() !== 'played games').map((lr: any) => lr.list_table_name);
-        for (let i = 0; i < tablesToDelete.length; i++) {
-          client.query('DROP TABLE IF EXISTS $1~;', tablesToDelete[i]).then(() => {
-            if (i === tablesToDelete.length - 1) return done();
-          })
-        }
+      rows.forEach((table: any, i: number) => {
+        client.query('DROP TABLE IF EXISTS $1~;', table.list_table_name).then(() => {
+          if (i === rows.length - 1) return done();
+        });
       });
     });
+  });
+
+  after(done => {
+    client.query('DELETE FROM users WHERE id = $1;', userId).then(() => done());
   });
 
   // create
