f256ee5f24362f87016cbfa5340aaa6f6547fa93
kyle-n <kylebnazario@gmail.com>
2019-03-24 13:11:36 -0400

    Error fixes

--- gt_frontend/src/components/header_box.js
+++ gt_frontend/src/components/header_box.js
@@ -27,30 +27,29 @@ class HeaderBoxCpt extends Component {
 class NavBar extends Component {
   constructor(props) {
     super(props);
-    const tabs = [
+    this.tabs = [
       { to: '/', tabName: 'Welcome' },
       { to: '/login', tabName: 'Log In' },
       { to: '/signup', tabName: 'Sign Up' }
     ];
-    this.tabMarkup = tabs.map(t => {
-      let active = false;
-      if (props.path.startsWith(t.to)) {
-        if (t.to === '/') active = t.to === props.path;
-        else active = true;
-      }
-      return (<NavTab key={t.to} active={active} to={t.to}>{t.tabName}</NavTab>);
-    });
     this.closeSidebar = this.closeSidebar.bind(this);
   }
   componentDidMount() {
     this.sidebar = M.Sidenav.init(document.querySelector('#mobile-nav'));
   }
   closeSidebar(e) {
-    console.log(e.target);
     if (e.target.tagName === 'A') this.sidebar.close();
   }
   render() {
     const circleButtonCss = { borderRadius: '50%', width: 'auto' };
+    const tabMarkup = this.tabs.map(t => {
+      let active = false;
+      if (this.props.path.startsWith(t.to)) {
+        if (t.to === '/') active = t.to === this.props.path;
+        else active = true;
+      }
+      return (<NavTab key={t.to} active={active} to={t.to}>{t.tabName}</NavTab>);
+    });
     return (
       <div>
         <nav className="nav-wrapper col s12">
@@ -59,12 +58,12 @@ class NavBar extends Component {
               <i className="material-icons z-depth-0">menu</i>
             </a>
             <ul id="nav-desktop" className="right hide-on-small-only">
-              {this.tabMarkup}
+              {tabMarkup}
             </ul>
           </div>
         </nav>
         <ul id="mobile-nav" onClick={this.closeSidebar} className="sidenav red lighten-2 white-text">
-          {this.tabMarkup}
+          {tabMarkup}
         </ul>
       </div>
     );
@@ -83,8 +82,8 @@ function SiteTitle(props) {
   return (
     <Link className="" to="/">
       <i className="">gamepad</i>
-      <div className="">{config.siteTitle}</div>
-      <h1 className="">{props.title}</h1>
+      <div className=""></div>
+      <h1 className="">{config.siteTitle}</h1>
     </Link>
   )
 }
