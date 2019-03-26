2a9324c56a55355fac2593668846e12a91cce80f
kyle-n <kylebnazario@gmail.com>
2019-03-26 16:10:10 -0400

    Better validation logic

--- gt_frontend/src/components/signup_page.js
+++ gt_frontend/src/components/signup_page.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import { config } from '../constants';
 import EmailChecker from './email_checker';
 import { debounce, validPassword } from '../utils';
+import { textSpanIntersectsWithTextSpan } from 'typescript';
 
 const mapStateToProps = state => {
   return {};
@@ -32,7 +33,7 @@ class SignupPage extends Component {
     this.setState({ email });
   }
   setPasswordField(oneOrTwo, value) {
-    const update = { valid: false, noMatch: true };
+    const update = { valid: false };
     update[oneOrTwo] = value;
     this.setState(update, () => {
       const invalidField = oneOrTwo === 'passwordOne' ? 'oneInvalid' : 'twoInvalid';
@@ -47,15 +48,18 @@ class SignupPage extends Component {
     const invalidField = oneOrTwo === 'passwordOne' ? 'oneInvalid' : 'twoInvalid';
     update[invalidField] = !validPassword(this.state[oneOrTwo]);
     const other = oneOrTwo === 'passwordOne' ? 'twoInvalid' : 'oneInvalid';
-    update.valid = !this.state.noMatch && !this.state[other] && !update[invalidField];
+    update.valid = !this.state.noMatch && !this.state[other] && !update[invalidField]
+      && this.state.passwordOne && this.state.passwordTwo && this.state.email;
     console.log(update, this.state[oneOrTwo]);
-    return this.setState(update);
+    return this.setState(update, () => {
+      if (oneOrTwo === 'passwordTwo') this.checkPwMatch();
+    });
   }
   checkPwMatch() {
     console.log('checking pw match');
     const update = {};
-    update.noMatch = this.state.passwordOne === this.state.passwordTwo;
-    update.valid = !update.noMatch && !this.state.oneInvalid && !this.state.twoInvalid;
+    update.noMatch = this.state.passwordOne !== this.state.passwordTwo;
+    update.valid = !update.noMatch && !this.state.oneInvalid && !this.state.twoInvalid && this.state.email;
     this.setState(update);
   }
   processSignup(e) {
@@ -78,8 +82,10 @@ class SignupPage extends Component {
 
 
 const SignupForm = props => {
-  const pwPattern = '.{6,}'
   const pwErrMsg = 'Passwords must have at least 6 characters and 1 number';
+  let pOneCss = '';
+  if (props.oneInvalid) pOneCss = 'invalid';
+  if (!props.oneInvalid && props.passwordOne) pOneCss = 'valid';
   return (
     <form onSubmit={props.submit} className="section">
       <p>{JSON.stringify(props)}</p>
@@ -88,7 +94,7 @@ const SignupForm = props => {
         <i className="material-icons prefix">vpn_key</i>
         <label htmlFor="signup-password">Password</label>
         <input type="password" id="signup-password" value={props.pwOne} onChange={e => props.setPw('passwordOne', e.target.value)} 
-          pattern={pwPattern} className={props.oneInvalid ? 'invalid' : ''} required
+          className={pOneCss} required
           onBlur={() => props.checkValidPw('passwordOne')} />
         <span className="helper-text" data-error={pwErrMsg}></span>
       </div>
@@ -96,10 +102,10 @@ const SignupForm = props => {
         <i className="material-icons prefix"></i>
         <label htmlFor="repeat-password">Password (again)</label>
         <input type="password" id="repeat-password" value={props.pwTwo} onChange={e => props.setPw('passwordTwo', e.target.value)} 
-          pattern={pwPattern} className={props} />
+          className={props.twoInvalid ? 'invalid' : '' } required onBlur={() => props.checkValidPw('passwordTwo')} />
         <span className="helper-text" data-error={pwErrMsg}></span>
+        <div className={props.noMatch ? 'helper-text red-text' : 'hide' }>Passwords must match</div>
       </div>
-      <span className={props.noMatch ? 'helper-text' : 'hide' } data-error="Passwords must match"></span>
       <button className="btn" type="submit" disabled={props.disabled}>Sign up<i className="material-icons right">send</i></button>
     </form>
   );
