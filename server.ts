a1759eb7ecc640b842b521f3dc556a08c2edf9da
kyle-n <kylebnazario@gmail.com>
2019-10-15 21:45:52 -0600

    Adds body parser

--- server.ts
+++ server.ts
@@ -2,7 +2,10 @@
 import * as express from 'express';
 import {config} from 'dotenv';
 import {join} from 'path';
+import {json} from 'body-parser';
 
+// app config
 const app = express();
+app.use(json());
 
-app.use((req, resp) => resp.send('Hello'))
+app.use((req, resp) => resp.send('Hello'));
