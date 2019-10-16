eff9e3e253e64cb46bffd513f565b1e9b119b186
kyle-n <kylebnazario@gmail.com>
2019-10-15 23:19:37 -0600

    User property inits

--- db/user.ts
+++ db/user.ts
@@ -1,4 +1,4 @@
-import {Model} from 'sequelize';
+import {Model, DataTypes} from 'sequelize';
 
 export class User extends Model {
   public id: number;
@@ -6,3 +6,26 @@ export class User extends Model {
   public password: string;
   public email: string;
 }
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  username: {
+    type: new DataTypes.STRING(32),
+    allowNull: false
+  },
+  password: {
+    type: new DataTypes.STRING(256),
+    allowNull: false
+  },
+  email: {
+    type: new DataTypes.STRING(),
+    allowNull: false
+  }
+}, {
+  tableName: 'users',
+  sequelize: null
+});
