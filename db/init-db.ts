00dade0f1818e2d9392c75ab172e129e9196522d
kyle-n <kylebnazario@gmail.com>
2019-10-16 19:35:11 -0600

    Initializes db connection

--- db/init-db.ts
+++ db/init-db.ts
@@ -1,5 +1,13 @@
-import {Observable} from 'rxjs';
+import {Observable, from, of} from 'rxjs';
+import {Sequelize} from 'sequelize';
 
-const initDatabase = (): Observable<void> => {
-  
+export function initializeDatabase(): Observable<Sequelize> {
+  const sequelize = new Sequelize(<string>process.env.PSQL_DB_NAME, <string>process.env.PSQL_USERNAME, process.env.PSQL_PASSWORD, {
+    host: 'localhost',
+    dialect: 'postgres'
+  });
+
+  const authTest: Observable<void> = from(sequelize.authenticate());
+
+  return of(sequelize);
 };
--- server.ts
+++ server.ts
@@ -1,13 +1,25 @@
 // npm
+import dotenv from 'dotenv';
 import express from 'express';
-import {config} from 'dotenv';
 import {join} from 'path';
 import {json} from 'body-parser';
+
+// submodules
+import {initializeDatabase} from './db';
+
+// hidden vars
+dotenv.config();
 
 // app config
 const app: express.Application = express();
 app.use(json());
 
-app.use((req, resp) => resp.send('Hello'));
+// init db
+initializeDatabase().subscribe(
+  () => console.log('Connected to Postgres'),
+  () => console.log('Could not connect to Postgres')
+);
 
+// test process
+app.use((req, resp) => resp.send('Hello'));
 app.listen(process.env.PORT || 8000, () => console.log(`Listening at port ${process.env.PORT || 8000}...`));
