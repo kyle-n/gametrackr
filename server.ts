37d1c11a31d496b66ac402a6c23a4f56f9d581e1
kyle-n <kylebnazario@gmail.com>
2019-10-16 19:12:36 -0600

    Fixes express import

--- server.ts
+++ server.ts
@@ -1,11 +1,11 @@
 // npm
-import * as express from 'express';
+import express from 'express';
 import {config} from 'dotenv';
 import {join} from 'path';
 import {json} from 'body-parser';
 
 // app config
-const app = express();
+const app: express.Application = express();
 app.use(json());
 
 app.use((req, resp) => resp.send('Hello'));
