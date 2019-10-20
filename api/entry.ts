3c74d3a0653266d3dedf072813c9d71b4fa2a513
kyle-n <kylebnazario@gmail.com>
2019-10-20 09:50:23 -0600

    Adds entry router

---
+++ api/entry.ts
@@ -0,0 +1,57 @@
+import express from 'express';
+
+import {validateRequest} from '../utils';
+
+import {List, Entry} from '../models';
+
+import * as postSchema from './schemas/entry/post.json';
+import * as patchSchema from './schemas/entry/patch.json';
+
+const validPost = (
+  req: express.Request,
+  resp: express.Response,
+  next: express.NextFunction,
+): void => validateRequest(req, resp, next, postSchema);
+
+const validPatch = (
+  req: express.Request,
+  resp: express.Response,
+  next: express.NextFunction,
+): void => validateRequest(req, resp, next, patchSchema);
+
+const router: express.Router = express.Router();
+
+router.post('/', validPost, async (req, resp) => {
+  const newEntry: Entry = await Entry.create({
+    text: req.body.text,
+    gameId: req.body.gameId,
+    listId: req.body.listId
+  });
+
+  return resp.json(newEntry.getPublicData());
+});
+
+router.get('/:id', async (req, resp) => {
+  const foundEntry: Entry | null = await Entry.findOne({where: {id: req.params.id}});
+  if (!foundEntry) return resp.status(404).send();
+
+  return resp.json(foundEntry.getPublicData());
+});
+
+router.patch('/:id', validPatch, async (req, resp) => {
+  const updatedEntries: Array<Entry> = (await Entry.update(req.body, {where: {id: req.params.id}}))[1];
+  if (!updatedEntries.length) return resp.status(404).send();
+  if (updatedEntries.length > 1) return resp.status(500).send();
+
+  return resp.json(updatedEntries[0].getPublicData());
+});
+
+router.delete('/:id', async (req, resp) => {
+  // check permissions
+  const entryToDelete: Entry | null = await Entry.findOne({where: {id: req.params.id}});
+  if (!entryToDelete) return resp.status(404).send();
+
+  return resp.status(200).send();
+});
+
+export default router;
--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import userRouter from './user';
 import listRouter from './list';
+import entryRouter from './entry';
 
 const apiRouters: express.Router = express.Router();
 
 apiRouters.use('/users', userRouter);
 apiRouters.use('/lists', listRouter);
+apiRouters.use('/entries', entryRouter);
 
 export default apiRouters;
