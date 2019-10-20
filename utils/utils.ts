81c80e7e4e67e05620b65467555f01ecba40fd28
kyle-n <kylebnazario@gmail.com>
2019-10-20 01:10:48 -0600

    Removes getpublicdatas from utils

--- utils/utils.ts
+++ utils/utils.ts
@@ -17,38 +17,3 @@ export const validateRequest: Function = (
   else resp.status(400).send('Invalid request: ' + validResult.errors);
 };
 
-interface PublicUserData {
-  id: number;
-  name: string;
-  email?: string;
-  createdAt: string;
-}
-
-export const getPublicUserData: Function = (user: User, returnEmail?: boolean): PublicUserData => {
-  const publicData: PublicUserData = {
-    id: user.id,
-    name: user.name,
-    createdAt: user.createdAt
-  };
-  if (returnEmail) publicData.email = user.email;
-  return publicData;
-}
-
-interface PublicListData {
-  id: number;
-  title: string;
-  createdAt: string;
-  updatedAt: string;
-  userName?: string;
-}
-
-export const getPublicListData: Function = (list: List, user?: User): PublicListData => {
-  const publicData: PublicListData = {
-    id: list.id,
-    title: list.title,
-    createdAt: list.createdAt,
-    updatedAt: list.updatedAt,
-  };
-  if (user) publicData.userName = user.name;
-  return publicData;
-}
