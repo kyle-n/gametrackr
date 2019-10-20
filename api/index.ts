242bb1c75e05c34509f0635a8e5fff960e489822
kyle-n <kylebnazario@gmail.com>
2019-10-20 12:50:37 -0600

    Adds game router

--- api/index.ts
+++ api/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import userRouter from './user';
 import listRouter from './list';
 import entryRouter from './entry';
+import gameRouter from './game';
 
 const apiRouters: express.Router = express.Router();
 
 apiRouters.use('/users', userRouter);
 apiRouters.use('/lists', listRouter);
 apiRouters.use('/entries', entryRouter);
+apiRouters.use('/games', gameRouter);
 
 export default apiRouters;
