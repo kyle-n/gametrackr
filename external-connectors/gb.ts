8176888edb5ead403102efce08c69c019642a92d
kyle-n <kylebnazario@gmail.com>
2019-10-20 12:51:01 -0600

    Finalizes gb game caching

--- external-connectors/gb.ts
+++ external-connectors/gb.ts
@@ -10,26 +10,26 @@ const mapGbGame = (game: GiantBombGame): any => {
     apiDetailUrl: game.api_detail_url || '',
     deck: game.deck || '',
     description: game.description || '',
-    developers: game.developers || [],
+    developers: game.developers || null,
     expectedReleaseDay: game.expected_release_day,
     expectedReleaseMonth: game.expected_release_month,
     expectedReleaseYear: game.expected_release_year,
-    franchises: game.franchises || [],
-    genres: game.genres || [],
+    franchises: game.franchises || null,
+    genres: game.genres || null,
     gbId: game.id,
     image: game.image.medium_url || '',
     name: game.name,
-    releases: game.releases || [],
+    releases: game.releases || null,
     siteDetailUrl: game.site_detail_url,
-    platforms: [],
-    ratings: [],
-    entries: []
   };
 }
 
 const upsertGames = async (games: Array<GiantBombGame>): Promise<void> => {
   const mappedGames: Array<any> = games.map(mapGbGame);
-  await Game.bulkCreate(mappedGames, {updateOnDuplicate: undefined});
+  await Game.bulkCreate(mappedGames, {updateOnDuplicate: [
+    'apiDetailUrl', 'deck', 'description', 'expectedReleaseDay', 'expectedReleaseMonth',
+    'expectedReleaseYear', 'image', 'name', 'siteDetailUrl'
+  ]});
 };
 
 const search = async (query: string): Promise<GiantBombResponse> => {
@@ -40,6 +40,9 @@ const search = async (query: string): Promise<GiantBombResponse> => {
     'resources=game'
   ].join('&');
   const gbResp: GiantBombResponse = <any>(await axios.get(url, {headers: [{accept: 'application/json'}], data: {}})).data;
+
+  // do not wait for database caching to return results to the user
+  upsertGames(gbResp.results);
 
   return gbResp;
 };
