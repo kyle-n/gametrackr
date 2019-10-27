874f3a66a4145be4fe82f95f3cd10836e9e0ca42
kyle-n <kylebnazario@gmail.com>
2019-10-27 13:30:00 -0600

    Cleanup and css

--- frontend/src/nav/hamburger-menu.js
+++ frontend/src/nav/hamburger-menu.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import {Drawer, IconButton, List, Divider, ListItem, ListItemIcon, ListItemText} from '@material-ui/core';
+import {Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText} from '@material-ui/core';
 import {
   Menu as MenuIcon,
 } from '@material-ui/icons';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
+import '../utils/layout.css';
 
 export class MobileNav extends React.Component {
   constructor(props) {
@@ -33,12 +33,12 @@ export class MobileNav extends React.Component {
 }
 
 const SideDrawerLink = props => (
-  <Link to={props.route.path}>
-    <ListItem>
-      <ListItemIcon>{props.route.icon}</ListItemIcon>
+  <NavLink to={props.route.path} className="unstyled-link">
+    <ListItem button>
+      <ListItemIcon><props.route.icon /></ListItemIcon>
       <ListItemText>{props.route.title}</ListItemText>
     </ListItem>
-  </Link>
+  </NavLink>
 );
 
 const SideDrawerContainer = props => {
@@ -61,105 +61,3 @@ const DrawerButton = props => (
     <MenuIcon />
   </IconButton>
 );
-
-// const useStyles = makeStyles({
-//   list: {
-//     width: 250,
-//   },
-//   fullList: {
-//     width: 'auto',
-//   },
-// });
-//
-// export function TemporaryDrawer() {
-//   const classes = useStyles();
-//   const [state, setState] = React.useState({
-//     top: false,
-//     left: false,
-//     bottom: false,
-//     right: false,
-//   });
-//
-//   const toggleDrawer = (side, open) => event => {
-//     if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-//       return;
-//     }
-//
-//     setState({ ...state, [side]: open });
-//   };
-//
-//   const sideList = side => (
-//     <div
-//       className={classes.list}
-//       role="presentation"
-//       onClick={toggleDrawer(side, false)}
-//       onKeyDown={toggleDrawer(side, false)}
-//     >
-//       <List>
-//         {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-//           <ListItem button key={text}>
-//             <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-//             <ListItemText primary={text} />
-//           </ListItem>
-//         ))}
-//       </List>
-//       <Divider />
-//       <List>
-//         {['All mail', 'Trash', 'Spam'].map((text, index) => (
-//           <ListItem button key={text}>
-//             <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-//             <ListItemText primary={text} />
-//           </ListItem>
-//         ))}
-//       </List>
-//     </div>
-//   );
-//
-//   const fullList = side => (
-//     <div
-//       className={classes.fullList}
-//       role="presentation"
-//       onClick={toggleDrawer(side, false)}
-//       onKeyDown={toggleDrawer(side, false)}
-//     >
-//       <List>
-//         {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-//           <ListItem button key={text}>
-//             <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-//             <ListItemText primary={text} />
-//           </ListItem>
-//         ))}
-//       </List>
-//       <Divider />
-//       <List>
-//         {['All mail', 'Trash', 'Spam'].map((text, index) => (
-//           <ListItem button key={text}>
-//             <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-//             <ListItemText primary={text} />
-//           </ListItem>
-//         ))}
-//       </List>
-//     </div>
-//   );
-//
-//   return (
-//     <div>
-//       <Button onClick={toggleDrawer('left', true)}>Open Left</Button>
-//       <Button onClick={toggleDrawer('right', true)}>Open Right</Button>
-//       <Button onClick={toggleDrawer('top', true)}>Open Top</Button>
-//       <Button onClick={toggleDrawer('bottom', true)}>Open Bottom</Button>
-//       <Drawer open={state.left} onClose={toggleDrawer('left', false)}>
-//         {sideList('left')}
-//       </Drawer>
-//       <Drawer anchor="top" open={state.top} onClose={toggleDrawer('top', false)}>
-//         {fullList('top')}
-//       </Drawer>
-//       <Drawer anchor="bottom" open={state.bottom} onClose={toggleDrawer('bottom', false)}>
-//         {fullList('bottom')}
-//       </Drawer>
-//       <Drawer anchor="right" open={state.right} onClose={toggleDrawer('right', false)}>
-//         {sideList('right')}
-//       </Drawer>
-//     </div>
-//   );
-// }
