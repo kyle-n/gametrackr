5b00499af3aa1b819878ec5c2453477a2ec2bad9
kyle-n <kylebnazario@gmail.com>
2019-10-27 22:01:26 -0600

    Mobile nav with title and sidebar

--- frontend/src/nav/title.js
+++ frontend/src/nav/title.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../utils/layout.css';
 import {MobileNav} from './hamburger-menu';
+import {Grid, Hidden} from '@material-ui/core';
 
 export class Title extends React.Component {
 
@@ -15,10 +16,20 @@ export class Title extends React.Component {
   render() {
     const routes = Title.getRoutesForLoginStatus(this.props.routes, this.props.loggedIn);
     return (
-      <header>
-        <h1>gametrackr</h1>
-        <MobileNav routes={routes} />
-      </header>
+      <Grid container alignItems="center">
+        <Grid item xs={1}>
+          <MobileNav routes={routes}/>
+        </Grid>
+        <Grid item xs={11}>
+          <SiteTitle/>
+        </Grid>
+      </Grid>
     );
   }
 }
+
+const SiteTitle = () => (
+  <header style={{textAlign: 'center'}}>
+    <h1>gametrackr</h1>
+  </header>
+);
