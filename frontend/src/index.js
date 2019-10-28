f5536396c340f014553ecc80d6ace4f6851267f3
kyle-n <kylebnazario@gmail.com>
2019-10-27 22:42:25 -0600

    Removes unused imports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {Themes} from './themes';
-import {CssBaseline} from '@material-ui/core';
 
 ReactDOM.render(
   (
