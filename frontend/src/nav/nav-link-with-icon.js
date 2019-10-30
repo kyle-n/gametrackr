29ef36f35ad2efacceaf5003956f5e0a5594b803
kyle-n <kylebnazario@gmail.com>
2019-10-29 22:34:03 -0600

    Nav links can run fn on click

--- frontend/src/nav/nav-link-with-icon.js
+++ frontend/src/nav/nav-link-with-icon.js
@@ -4,7 +4,7 @@ import React from 'react';
 import '../utils/layout.css';
 
 export const NavLinkWithIcon = props => (
-  <NavLink to={props.route.path} className="unstyled-link" onClick={props.route.fn ? props.route.fn : null}>
+  <NavLink to={props.route.path} className="no-underline reset-color" onClick={props.route.fn ? props.route.fn : null}>
     <ListItem>
       <ListItemIcon style={{minWidth: props.desktop ? '33px' : '60px'}}><props.route.icon /></ListItemIcon>
       <ListItemText style={{marginRight: props.desktop ? '0' : '60px'}}>{props.route.title}</ListItemText>
