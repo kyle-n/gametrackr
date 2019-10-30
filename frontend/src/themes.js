273756aaea4c5cf06db360a0f1eeafc7afd3b884
kyle-n <kylebnazario@gmail.com>
2019-10-29 22:34:44 -0600

    Basic themepicker

--- frontend/src/themes.js
+++ frontend/src/themes.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
+import {Button, Menu, MenuItem} from '@material-ui/core';
+import {Palette as PaletteIcon} from '@material-ui/icons';
 
 const dark = createMuiTheme({
   palette: {
@@ -20,3 +22,37 @@ export const Themes = props => (
     {props.children}
   </ThemeProvider>
 );
+
+export class ThemeMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  toggleOpen = event => {
+    this.anchorRef = event.target;
+    this.setState({open: !this.state.open});
+  };
+
+  render() {
+    const menuItems = Object.keys(themes).map(themeName => {
+      return (
+        <MenuItem key={themeName} onClick={() => this.props.setTheme(themes[themeName])}>
+          {themeName}
+        </MenuItem>
+      );
+    });
+    return (
+      <div>
+        <Button variant="outlined" onClick={this.toggleOpen}>
+          <PaletteIcon />
+          Theme
+        </Button>
+        <Menu open={this.state.open} anchorEl={this.anchorRef} onClose={this.toggleOpen}>
+          {menuItems}
+        </Menu>
+      </div>
+    );
+  }
+
+}
