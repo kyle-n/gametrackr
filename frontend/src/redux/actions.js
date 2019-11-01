ea23b480af6a0e9f218cf8113793eecd4363a6b1
kyle-n <kylebnazario@gmail.com>
2019-10-31 21:30:59 -0600

    Action generators

---
+++ frontend/src/redux/actions.js
@@ -0,0 +1,13 @@
+import {SET_THEME, SET_JWT, SET_USER_ID} from './action-types';
+
+export function setTheme(theme) {
+  return {type: SET_THEME, theme};
+}
+
+export function setJwt(jwt) {
+  return {type: SET_JWT, jwt};
+}
+
+export function setUserId(userId) {
+  return {type: SET_USER_ID, userId};
+}
