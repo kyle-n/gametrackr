eb8d754b0a80a79eb89480af56b43d9d78764d79
kyle-n <kylebnazario@gmail.com>
2019-11-02 23:19:00 -0600

    Simpler form layout

--- frontend/src/user/signup-page.js
+++ frontend/src/user/signup-page.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import {Grid} from '@material-ui/core';
+import {Divider, Grid, Hidden} from '@material-ui/core';
 import SignupFormContainer from './signup-form-container';
+import {Link} from 'react-router-dom';
 
 const SignupPage = props => (
   <Grid container>
     <Grid item xs={12}>
       <h2>Sign up for gametrackr</h2>
     </Grid>
-    <Grid item xs={12} md={8}>
+    <Grid item xs={12} md={7}>
       <SignupFormContainer />
     </Grid>
-    <Grid item xs={12} md={4}>
-      <p>Or log in</p>
+    <Grid item xs={12} >
+      <LoginMessage />
     </Grid>
   </Grid>
+);
+
+const LoginMessage = () => (
+  <p>Already have an account? <Link to='/login' className="reset-color">Log in here</Link>.</p>
 );
 
 export default SignupPage;
