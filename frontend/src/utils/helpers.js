a7e1100b11c438dddd04f2e1f492b642efe1a949
kyle-n <kylebnazario@gmail.com>
2019-11-03 01:05:18 -0600

    email regex

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -2,3 +2,5 @@ export const upperCaseFirstLetter = str => {
   if (typeof str !== 'string') return '';
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
+
+export const isEmail = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
