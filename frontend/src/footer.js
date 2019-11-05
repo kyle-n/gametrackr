e334dd6e0759edf592193ff3239bcf0268c8f66b
kyle-n <kylebnazario@gmail.com>
2019-11-04 22:29:59 -0700

    Cleans up imports on footer

--- frontend/src/footer.js
+++ frontend/src/footer.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import {Divider, Grid} from '@material-ui/core';
+import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
 export const Footer = props => (
-  <footer style={{marginTop: '3rem', textAlign: 'center'}}>
+  <footer style={{textAlign: 'center'}}>
     <Grid container justify="center">
       <Grid item xs={6}>
-        <Divider />
+        <Divider style={{margin: '3rem 0 1rem 0'}} />
       </Grid>
       <Grid item xs={12}>
-        <p>
+        <Typography variant="p">
           Made by <a href="https://www.kylenazario.com/" target="_blank" className="reset-color">Kyle Nazario</a>.
           See the <a href="https://github.com/kyle-n/gametrackr" target="_blank" className="reset-color">source code</a>.
-        </p>
+        </Typography>
       </Grid>
     </Grid>
   </footer>
