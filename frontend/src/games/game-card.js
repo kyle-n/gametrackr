6e8d1e4ed5c0dcd6192e4d70e0a48284672460aa
kyle-n <kylebnazario@gmail.com>
2019-11-04 21:56:29 -0700

    First draft of card to display game results

---
+++ frontend/src/games/game-card.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Grow from '@material-ui/core/Grow';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import {CardActions} from '@material-ui/core';
+import {gamePropsToDate} from '../utils';
+
+const GameCard = props => (
+  <Grow in>
+    <Card>
+      <CardHeader title={props.game.name}
+                  subheader={gamePropsToDate(props.game).toDateString()}
+      >
+      </CardHeader>
+      <CardMedia image="https://material-ui.com/static/images/cards/paella.jpg"
+                 title={props.game.name}
+      />
+      <CardContent>
+        {props.game.deck}
+      </CardContent>
+      <CardActions>
+      </CardActions>
+    </Card>
+  </Grow>
+);
+
+export default GameCard;
