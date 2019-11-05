9731f41399a23cc9e8b8c5f164776f2b1cbc53c4
kyle-n <kylebnazario@gmail.com>
2019-11-04 21:20:53 -0700

    Loading spinner on search input

--- frontend/src/search/search-input.js
+++ frontend/src/search/search-input.js
@@ -4,7 +4,9 @@ import Input from '@material-ui/core/Input';
 import FormControl from '@material-ui/core/FormControl';
 import Grid from '@material-ui/core/Grid';
 import SearchIcon from '@material-ui/icons/Search';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import {debounce} from 'throttle-debounce';
+import {Hidden} from '@material-ui/core';
 
 const SearchInput = props => {
   const debouncedSetQuery = debounce(1000, props.setQuery);
@@ -18,6 +20,7 @@ const SearchInput = props => {
       <Grid item xs={11}>
         <SearchFormControl searchType={props.searchType}
                            setQuery={debouncedSetQuery}
+                           loading={props.loading}
         />
       </Grid>
     </Grid>
@@ -36,9 +39,21 @@ const SearchFormControl = props => {
              name={'Search ' + props.searchType}
              autoFocus
              onChange={e => props.setQuery(e.target.value)}
+             endAdornment={(<Spinner loading={props.loading} />)}
       />
     </FormControl>
   );
 };
+
+const Spinner = props => (
+  <span>
+    {props.loading ? (
+      <CircularProgress size={'1.25rem'}
+                        style={{marginBottom: '0.5rem', marginRight: '0.5rem'}}
+                        color="secondary"
+      />
+    ) : null}
+  </span>
+);
 
 export default SearchInput;
