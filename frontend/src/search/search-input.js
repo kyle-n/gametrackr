d61c2ed7faface583caabbcdf230f08339247f27
kyle-n <kylebnazario@gmail.com>
2019-11-04 20:56:30 -0700

    Refactors input to be purely functional/display

--- frontend/src/search/search-input.js
+++ frontend/src/search/search-input.js
@@ -6,30 +6,23 @@ import Grid from '@material-ui/core/Grid';
 import SearchIcon from '@material-ui/icons/Search';
 import {debounce} from 'throttle-debounce';
 
-export default class SearchInput extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.debouncedSetQuery = debounce(1000, props.setQuery);
-  }
-
-  render() {
-    return (
-        <Grid container>
-        <Grid item xs={1}
-              style={{textAlign: 'center', margin: 'auto 0 0 0'}}
-        >
-          <SearchIcon />
-        </Grid>
-        <Grid item xs={11}>
-          <SearchFormControl searchType={this.props.searchType}
-                             setQuery={this.debouncedSetQuery}
-          />
-        </Grid>
+const SearchInput = props => {
+  const debouncedSetQuery = debounce(1000, props.setQuery);
+  return (
+      <Grid container>
+      <Grid item xs={1}
+            style={{textAlign: 'center', margin: 'auto 0 0 0'}}
+      >
+        <SearchIcon />
       </Grid>
-    );
-  }
-}
+      <Grid item xs={11}>
+        <SearchFormControl searchType={props.searchType}
+                           setQuery={debouncedSetQuery}
+        />
+      </Grid>
+    </Grid>
+  );
+};
 
 const SearchFormControl = props => {
   const inputId = props.searchType + '-search-input';
@@ -47,3 +40,5 @@ const SearchFormControl = props => {
     </FormControl>
   );
 };
+
+export default SearchInput;
