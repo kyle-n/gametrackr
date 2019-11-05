93994221dc67775943bad1dd53dfc7a013420fda
kyle-n <kylebnazario@gmail.com>
2019-11-04 21:21:15 -0700

    Moves network request and stateful ops to container

--- frontend/src/search/search-container.js
+++ frontend/src/search/search-container.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import SearchGameInput from './search-input';
+import SearchInput from './search-input';
+import {searchGame} from '../external-connectors';
 
 class SearchContainer extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      searchResults: [],
+      loading: false
+    };
   }
+
+  searchGames = query => {
+    this.setState({loading: true}, async () => {
+      const resp = await searchGame(query);
+      this.setState({searchResults: resp.games, loading: false});
+    });
+  };
+
   render() {
     return (
-      <SearchGameInput />
+      <SearchInput searchType="games"
+                   setQuery={this.searchGames}
+                   loading={this.state.loading}
+      />
     );
   }
 }
