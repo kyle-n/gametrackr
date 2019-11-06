cffd02d467660ae4709369d2cfed52f6d317cd08
kyle-n <kylebnazario@gmail.com>
2019-11-05 22:16:57 -0700

    Grid container for search results

--- frontend/src/search/game-results-box.js
+++ frontend/src/search/game-results-box.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 import {GameCard} from '../games';
 import '../utils/layout.css';
 
 const GameResultsBox = props => (
   <section className="large-margin">
-    {props.games.map(game => {
-      return (
-        <GameCard key={game.gbId} game={game} />
-      );
-    })}
+  	<Grid container>
+	    {props.games.map(game => {
+	      return (
+	        <GameCard key={game.gbId} game={game} />
+	      );
+	    })}
+	  </Grid>
   </section>
 );
 
