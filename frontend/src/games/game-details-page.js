ff46045afa50395c9c1787fb1ac88ce7447d03a6
kyle-n <kylebnazario@gmail.com>
2019-11-06 21:37:55 -0700

    Page title and inserts GB html

--- frontend/src/games/game-details-page.js
+++ frontend/src/games/game-details-page.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Typography from '@material-ui/core/Typography';
 import '../utils/layout.css';
+import {PageTitle} from '../utils';
 
 const GameDetailsPage = props => {
 	return props.game && props.game.description ? (
-			<article>
-				{props.game.description}
-			</article>
+			<GameDetails game={props.game} />
 		) : (
+			<GamePageLoadingSpinner />
+		);
+}
+
+const GameDetails = props => {
+	return (
+		<article>
+			<PageTitle title={props.game.name} />
+			<div dangerouslySetInnerHTML={{__html: props.game.description}}></div>
+		</article>
+	);
+};
+
+const GamePageLoadingSpinner = () => (
 			<div style={{textAlign: 'center'}} className="large-margin">
 					<CircularProgress color="secondary"
 														size="10%" />
 			</div>
-		);
-	}
+);
 
 export default GameDetailsPage;
