0daaf67b1260c91e3ba751495bdc548d52e90587
kyle-n <kylebnazario@gmail.com>
2019-11-10 13:19:54 -0700

    Dropdown markup for overlay

--- frontend/src/user/login-dropdown.js
+++ frontend/src/user/login-dropdown.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
+import Grow from '@material-ui/core/Grow';
 import Hidden from '@material-ui/core/Hidden';
 import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
 import LoginFormContainer from './login-form-container';
 
 const LoginDropdown = props => (
   <Hidden smDown>
-    <Grid container>
-      <Grid item md={4}>
-        <LoginFormContainer />
-      </Grid>
-    </Grid>
+    <Grow in style={{transformOrigin: 'top left'}}>
+      <Box position="absolute"
+          mt="0.5rem">
+        <Box position="relative"
+            zIndex="1"
+            minWidth="20rem">
+          <LoginFormCard onLogin={props.onLogin} />
+        </Box>
+      </Box>
+    </Grow>
   </Hidden>
+);
+
+const LoginFormCard = props => (
+  <Card>
+    <CardContent>
+      <LoginFormContainer onLogin={props.onLogin} />
+    </CardContent>
+  </Card>
 );
 
 export default LoginDropdown;
