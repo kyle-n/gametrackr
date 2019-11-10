bfbb52a322cf473359e984ca291599b0316b6cef
kyle-n <kylebnazario@gmail.com>
2019-11-10 13:19:20 -0700

    Splits up markup for reuse

--- frontend/src/nav/nav-link-with-icon.js
+++ frontend/src/nav/nav-link-with-icon.js
@@ -6,10 +6,16 @@ import React from 'react';
 import '../utils/layout.css';
 
 export const NavLinkWithIcon = props => (
-  <NavLink to={props.route.path} className="no-underline reset-color" onClick={props.route.fn ? props.route.fn : null}>
-    <ListItem>
-      <ListItemIcon style={{minWidth: props.desktop ? '33px' : '60px'}}><props.route.icon /></ListItemIcon>
-      <ListItemText style={{marginRight: props.desktop ? '0' : '60px'}}>{props.route.title}</ListItemText>
-    </ListItem>
+  <NavLink to={props.route.path} className="no-underline reset-color">
+    <ListItemTextAndIcon desktop={props.desktop}
+                         route={props.route}
+                         onClick={props.route.fn} />
   </NavLink>
+);
+
+export const ListItemTextAndIcon = props => (
+  <ListItem onClick={props.onClick ? props.onClick : null}>
+    <ListItemIcon style={{minWidth: props.desktop ? '33px' : '60px'}}><props.route.icon /></ListItemIcon>
+    <ListItemText style={{marginRight: props.desktop ? '0' : '60px'}}>{props.route.title}</ListItemText>
+  </ListItem>
 );
