55167d772d6c0167749b1bb0329b2f24f69962b7
kyle-n <kylebnazario@gmail.com>
2019-11-10 14:12:01 -0700

    Places container in page

--- frontend/src/lists/user-lists-page.js
+++ frontend/src/lists/user-lists-page.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import UserListsContainer from './user-lists-container';
+import {PageTitle} from '../utils';
 
 const UserListsPage = () => (
-  <p>page</p>
+  <div>
+    <PageTitle title="My lists" />
+    <UserListsContainer />
+  </div>
 );
 
 export default UserListsPage;
