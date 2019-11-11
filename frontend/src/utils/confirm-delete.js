dfd9dded7a94c0d78fd7e07c6cc90d3426fb6810
kyle-n <kylebnazario@gmail.com>
2019-11-11 06:09:47 -0700

    Delete confirmation popup

---
+++ frontend/src/utils/confirm-delete.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+class ConfirmDelete extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {open: props.open};
+  }
+
+  closeDialog = () => this.setState({open: false});
+
+  render() {
+    return (
+      <Dialog open={this.state.open}
+              onClose={this.closeDialog}>
+        <DialogTitle id="confirm-delete-dialog">
+          Confirm delete
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure you want to delete {this.props.entityType} {this.props.entityId}?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button>
+            Cancel
+          </Button>
+          <Button color="secondary">
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+}
+
+export default connect()(ConfirmDelete);
--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,5 +1,6 @@
 export {default as PageTitle} from './page-title';
 export {default as SubmitButton} from './submit-button';
 export {default as GtFormControl} from './gt-form-control';
+export {default as ConfirmDelete} from './confirm-delete';
 export * from './layout.css';
 export * from './helpers';
