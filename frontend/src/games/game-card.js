7f64626df084f3725f497bb979aaf93f6abd81dc
kyle-n <kylebnazario@gmail.com>
2019-11-10 21:47:00 -0700

    Splits game card into manageable components

--- frontend/src/games/game-card.js
+++ frontend/src/games/game-card.js
@@ -6,7 +6,6 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
 import CardActions from '@material-ui/core/CardActions';
 import {Link} from 'react-router-dom';
 import {gamePropsToDate} from '../utils';
@@ -28,26 +27,38 @@ const GameCard = props => {
           {props.game.deck}
         </CardContent>
         <CardActions>
-          <Grid container>
-            <Grid item xs={12} sm={6}>
-              {props.game.description ? (
-                <Link to={`/games/${props.game.gbId}/details`}>
-                  <Button size="small" color="secondary">
-                    Details
-                  </Button>
-                </Link>
-              ) : null}
-            </Grid>
-            <Grid item xs={12} sm={6} style={{textAlign: 'right'}}>
-              <Button size="small" color="secondary">
-                Add to list
-              </Button>
-            </Grid>
-          </Grid>
+          <GameCardActions game={props.game} />
         </CardActions>
       </Card>
     </Grow>
   );
 };
+
+const GameCardActions = props => (
+  <Grid container>
+    <Grid item xs={12} sm={6}>
+      {props.game.description ? (
+        <LinkToDescription game={props.game} />
+      ) : null}
+    </Grid>
+    <Grid item xs={12} sm={6} style={{textAlign: 'right'}}>
+        <AddToList />
+    </Grid>
+  </Grid>
+);
+
+const LinkToDescription = props => (
+  <Link to={`/games/${props.game.gbId}/details`}>
+    <Button size="small" color="secondary">
+      Details
+    </Button>
+  </Link>
+);
+
+const AddToList = props => (
+  <Button size="small" color="secondary">
+    Add to list
+  </Button>
+);
 
 export default GameCard;
